e72bdcfdbb413d431c80070ec96210a628fcf77f
denizafontineles <denizafontineles@outlook.com>
2019-07-22 20:54:15 -0300

    Mostrar novo padrão do JavaScript no jogo da forca

--- script.js
+++ script.js
@@ -1,26 +1,134 @@
-const palavra = "deniza"
+let palavra = "";
+let dica = "";
+const palavras = [
+    {
+        palavra: "BANANA",
+        dica: "Fruta amarela"
+    }, 
+    { 
+        palavra: 'QWERTY',
+        dica: 'Coisa do teclado'
+    }, 
+    {
+        palavra: 'INCONSTITUCIONALISSIMAMENTE',
+        dica: 'Vei, si fudeu'
+    }
+];
 let letrasErradas = []
 let letrasAcertadas = []
-function tentativa(botao){
+let caracteres = '';
+const corpoArray = [
+    "corpo2", "corpo4", "corpo3", "corpo5", "corpo6"
+]
+window.onload = function () {
+    inicializa();
+}
+
+function renderizar() {
+    document.getElementById('palavra').innerHTML = caracteres.split("").join(" ");
+}
+
+function sortearPalavra() {
+    let objeto = palavras[Math.floor(Math.random() * palavras.length)];
+    palavra = objeto.palavra;
+    dica = objeto.dica;
+}
+
+function inicializa() {
+    sortearPalavra();
+    esconderCorpo();
+    document.getElementById('novamente').disabled = true;
+    document.getElementById('chances').innerHTML = tratamentoStringChances(5);
+    for (let letra in palavra) {
+        caracteres += '_'
+    }
+    renderizar();
+}
+
+function tentativa(botao) {
     botao.disabled = true
     const letra = botao.innerText.toLowerCase()
     const temLetra = (palavra.toLowerCase().indexOf(letra) !== -1)
-
-    if(!temLetra)
-        letrasErradas.push(letra)
-    if(temLetra)
-        letrasAcertadas.push(letra)
-     
-    if(letrasAcertadas.length == palavra.length || letrasErradas.length >= 6)
+    
+    if (temLetra) {
+        substituiLetra(letra)
+        letrasAcertadas.push(letra);
+    } else if (!temLetra) {
+        renderizarErro(letra)
+    }
+    
+    if (letrasAcertadas.length == palavra.length || letrasErradas.length >= 5) {
         desabilitarBotoes()
-
-    console.log(letrasAcertadas)
+        document.getElementById('novamente').disabled = false;
+    }
+    for(indice of palavras){
+        console.log(indice)
+        console.log(caracteres)
+        if (indice == caracteres){
+            alert ("PARABÉNS, VOCÊ GANHOU!!")
+        }
+    }
 }
 
-function desabilitarBotoes(){
+function renderizarErro(letra) {
+    letrasErradas.push(letra);
+    const quantidadeChances = (5 - letrasErradas.length);
+    document.getElementById('chances').innerHTML = tratamentoStringChances(quantidadeChances);
+    renderizaCorpo(quantidadeChances);
+}
+
+function renderizaCorpo(chance) {
+    const parteDoCorpo = corpoArray[Math.abs(4 - chance)];
+    console.log(parteDoCorpo)
+    document.getElementById('cabeça').style.display = 'inline';
+    document.querySelector('#cabeça img').src = "imagens/" + parteDoCorpo + ".png"
+
+}
+function esconderCorpo() {
+    for (let item in corpoArray) {
+        const parteDoCorpo = corpoArray[item];
+        document.getElementById(parteDoCorpo).style.display = 'none';
+    }
+}
+
+function tratamentoStringChances(quantidade) {
+    return "Você tem ".concat(quantidade).concat(" chances")
+}
+
+function trocaStringApartirDaPosicao(str, index, chr) {
+    if (index > str.length - 1) return str;
+    return str.substr(0, index) + chr + str.substr(index + 1);
+}
+
+function substituiLetra(letraInformada) {
+    let posicoes = [];
+    for (let item in palavra) {
+        const letraNaPlavra = palavra[item];
+        if (letraNaPlavra.toLowerCase() == letraInformada.toLowerCase()) {
+            posicoes.push(item);
+        }
+    }
+    let caracteresArray = caracteres.split("");
+    for (let index in caracteresArray) {
+        if (posicoes.includes(index)) {
+            caracteresArray[index] = letraInformada.toUpperCase();
+        }
+    }
+    caracteres = caracteresArray.join("");
+    renderizar()
+}
+
+function desabilitarBotoes() {
 
     const buttons = document.getElementsByClassName('letra');
-    for (var i = 0; i < buttons.length; i++) {
-    buttons[i].disabled= true;
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
     }
+}
+
+function atualizaPagina() {
+    location.reload();
+}
+
+function gameOver() {
 }
